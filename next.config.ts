19b5daac27dbfcd254ce2e468a0b20ec34ee6de8
jordi85 <122380684+jordi85@users.noreply.github.com>
2024-10-25 19:15:44 -0500

    Cabecera detall1e

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-// next.config.js
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Otras opciones de configuración...
-
-  eslint: {
-    // Advertencia: Esto permite que el build se complete incluso si hay errores de ESLint.
-    // No es recomendado para producción, ya que puede ocultar errores importantes.
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
